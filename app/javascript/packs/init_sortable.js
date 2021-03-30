89825367b7384b9ba3cb510720e5648e5200909a
Kevin Abergel <k.abergel91@gmail.com>
2021-03-30 15:07:55 +0200

    sortable prettier

--- app/javascript/packs/init_sortable.js
+++ app/javascript/packs/init_sortable.js
@@ -2,7 +2,12 @@ import Sortable from 'sortablejs';
 
 const initSortable = () => {
   const list = document.querySelector('#results');
-  Sortable.create(list);
+  Sortable.create(list, {
+    ghostClass: "ghost",
+    dragClass: "drag",
+    forceFallback: true,
+    animation: 200
+  });
 };
 
 export { initSortable };
